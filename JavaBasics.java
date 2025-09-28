02f2d5e470a2064d6fad67d554ad2df8f39df1e1
Ritesh Yadav <riteshyadav2024@gmail.com>
2025-09-28 17:54:38 +0530

    java basics

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,14 +1,14 @@
-//public class JavaBasics {
-   // public static void main(String args[]) {
-       // System.out.print("hello world");
-       //// System.out.println("hello world");
-        //System.out.println("ritesh\n kumar\n yadav");
-      //  System.out.println("****");
-      //  System.out.println("***");
-      //  System.out.println("**");
-      //  System.out.println("*");
-  //  }   
-//}     
+/*public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.print("hello world");
+        System.out.println("hello world");
+        System.out.println("ritesh\n kumar\n yadav");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+            }   
+}*/     
 
 
 
@@ -16,28 +16,28 @@
 
 //variable in java
 
-//public class JavaBasics {
-  //  public static void main(String args[]){
-    //    int a = 10;
-      //  int b= 20;
-        //System.out.println(a); 
-        //System.out.println(b);
-        //String name = "ritesh";
-        //System.out.println(name);
+/*public class JavaBasics {
+    public static void main(String args[]){
+        int a = 10;
+        int b= 20;
+        System.out.println(a); 
+        System.out.println(b);
+        String name = "ritesh";
+        System.out.println(name);
 
 
-        //a =30;
-        //b=40;
-      //  System.out.println(ab);
-    //    System.out.println(b);
-  //  }
-//}
+        a =30;
+        b=40;
+        System.out.println(ab);
+        System.out.println(b);
+    }
+}*/
 
 
 
 //data types in java
 
-public class JavaBasics {
+/*  public class JavaBasics {
     public static void main(String args[]){
        byte b =5;
        System.out.println(b);
@@ -53,4 +53,141 @@ public class JavaBasics {
         
 
     } 
-}
+}*/
+
+
+
+
+// SUM OF A & B
+/*public class JavaBasics {
+    public static void main(String args[]){
+        int a=10;
+        int b=20;
+        int sum =a + b;
+        System .out.println(sum);
+
+    }
+}*/
+
+//INPUT IN JAVA
+
+/* 
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       // String input = sc.next();
+        //System.out.println("you have entered " + input);
+       // String name = sc.nextLine();
+       // System.out.println(name);
+
+       // int number = sc.nextInt();
+        //System.out.println(number);
+
+       // float price = sc.nextFloat();
+        //System.out.println(price);
+
+       // double d = sc.nextDouble();
+       // System.out.println(d);
+
+       // boolean b = sc.nextBoolean();
+        //System.out.println(b);  
+
+        
+        
+
+    }
+}  */
+
+
+
+
+
+
+//SUM OF 2 NUMBERS TAKEN FROM USER
+
+/* 
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("the sum of a and b is " + sum);
+    }
+
+}  */
+
+
+
+//PRODUCT OF 2 NUMBERS TAKEN FROM USER
+
+/*import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println("the product of a and b is " + product);
+    }
+} */
+
+
+
+//AREA OF CIRCLE
+/*import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        float r = sc.nextFloat();
+        float area = 3.14f * r * r;
+        System.out.println("the area of circle is " + area);
+    }
+}*/
+
+
+
+
+// TYPE CONVERSION IN JAVA
+/* 
+import java.util.*;
+
+public class JavaBasics{
+    public static void main(String args[]){
+       // int a = 257;
+      //  long b= a;
+      //  System.out.println(b);
+      Scanner sc =new Scanner(System.in);
+      float number = sc.nextFloat();
+      System.out.println(number);
+
+
+
+    }
+}    */
+
+
+
+//TYPE CASTING IN JAVA
+
+/* 
+import java.util.*; 
+  // public class JavaBasics{
+    public static void main(String args[]){
+       // Scanner sc= new Scanner (System.in);
+        //float a= 25.5f;
+        //int b = (int)a;
+        //System.out.println(b);
+        char ch = 'a';
+        int number = ch;
+        System.out.println(number);
+    }
+}*/
+
+
+
