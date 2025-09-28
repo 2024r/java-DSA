e95dba926fe96a1158b21b71fd2e3945c5cc80bb
Ritesh Yadav <riteshyadav2024@gmail.com>
2025-09-28 14:57:42 +0530

    java basics

---
+++ JavaBasics.java
@@ -0,0 +1,56 @@
+//public class JavaBasics {
+   // public static void main(String args[]) {
+       // System.out.print("hello world");
+       //// System.out.println("hello world");
+        //System.out.println("ritesh\n kumar\n yadav");
+      //  System.out.println("****");
+      //  System.out.println("***");
+      //  System.out.println("**");
+      //  System.out.println("*");
+  //  }   
+//}     
+
+
+
+
+
+//variable in java
+
+//public class JavaBasics {
+  //  public static void main(String args[]){
+    //    int a = 10;
+      //  int b= 20;
+        //System.out.println(a); 
+        //System.out.println(b);
+        //String name = "ritesh";
+        //System.out.println(name);
+
+
+        //a =30;
+        //b=40;
+      //  System.out.println(ab);
+    //    System.out.println(b);
+  //  }
+//}
+
+
+
+//data types in java
+
+public class JavaBasics {
+    public static void main(String args[]){
+       byte b =5;
+       System.out.println(b);
+       char ch = 'a';
+       System.out.println(ch);
+       boolean var = false;
+       double price = 10.5;
+       int number = 100;
+       short n = 240;
+       long n1 = 123456; 
+        float f1 = 12.5f;
+        System.out.println(f1);
+        
+
+    } 
+}
